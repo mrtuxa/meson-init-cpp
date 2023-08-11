e7a549260507cc25c8d9d746c32cca42241902ea
mrtuxa <mrtux@infra-sys.de>
2023-08-11 15:53:42 +0200

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# meson-init-cpp
+meson-init-cpp is a project initialization tool for C++ projects using the Meson build system.
+
+# Installation
+To install meson-init-cpp, follow these steps:
+
+Clone the repository: git clone https://github.com/your-repo/meson-init-cpp.git
+Navigate to the project directory: cd meson-init-cpp
+Build the project: meson build
+Install meson-init-cpp: make
+
+# Usage
+To use meson-init-cpp, run the following command:
+
+
+```bash
+meson-init-cpp <project_name> <version> <c++ version>
+```
+
+For example:
+
+meson-init-cpp foo 0.1 14
+
+
+This will create a new project with the specified name, version, and C++ version.
+
+Contributing
+Contributions to meson-init-cpp are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request on the GitHub repository.
+
+License
+meson-init-cpp is licensed under the MIT License. See the LICENSE file for more information.
+
+Contact
+For any questions or further information, you can reach out to me via email or through the GitHub repository.
---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  if (argc < 4) {
+    std::cout
+        << "Usage: <project_name> <version> <c++ version>\nExample: foo 0.1 14"
+        << std::endl;
+    exit(1);
+  } else if (argc == 4) {
+    std::filesystem::path currentDir = std::filesystem::current_path();
+    if (!std::filesystem::is_empty(currentDir)) {
+      std::cout << "The directory must be empty!" << std::endl;
+      exit(1);
+    }
+    std::ofstream project("meson.build");
+
+    // create meson.build file
+    project << "project('" << argv[1] << "', 'cpp',\n"
+            << "\tversion : '" << argv[2] << "',\n"
+            << "\tdefault_options : ['warning_level=3',\n"
+            << "\t\t\t   'cpp_std=c++" << argv[3] << "'])\n\n"
+            << "exe = executable('" << argv[1] << "', 'src/main.cpp',\n"
+            << "\tinstall : true)\n\n"
+            << "test('basic', exe)\n";
+
+    // create source dir
+    std::string src = "src";
+    std::filesystem::create_directory(src);
+    std::filesystem::permissions(src, std::filesystem::perms::others_all,
+                                 std::filesystem::perm_options::remove);
+
+    // create example file
+    std::ofstream main("src/main.cpp");
+
+    main << "#include <iostream>" << std::endl
+         << std::endl
+         << "int main() {" << std::endl
+         << "  std::cout <<  \"Hello World\" << std::endl;" << std::endl
+         << std::endl
+         << "  return 0;" << std::endl
+         << "}" << std::endl;
+
+    std::cout << "Source Directory" << std::endl;
+    for (const auto &entry : std::filesystem::directory_iterator(src)) {
+      std::cout << entry.path() << std::endl;
+    }
+  }
+
+  // create Makefile
+  std::ofstream makefile("Makefile");
+
+  makefile << "all: clean setup compile" << std::endl
+           << "default: all" << std::endl
+           << std::endl
+           << "setup:" << std::endl
+           << "\t@meson setup builddir" << std::endl
+           << std::endl
+           << "compile:" << std::endl
+           << std::endl
+           << std::endl
+           << "\t@meson compile -C builddir" << std::endl
+           << "clean:" << std::endl
+           << "\t@rm -f -R builddir" << std::endl;
+
+  return 0;
+}
